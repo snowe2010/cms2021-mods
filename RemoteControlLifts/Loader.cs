653de3ad362bd051b79df8d2c54ba473c800b22a
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2021-09-06 00:14:35 -0600

    Add new RemoteControlLifts mod

---
+++ RemoteControlLifts/Loader.cs
@@ -0,0 +1,22 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace RemoteControlLifts
+{
+    public class Loader : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg("Loading preferences");
+            var melonPreferencesCategory = MelonPreferences.CreateCategory("BetterWarehouse");
+            if (!melonPreferencesCategory.HasEntry("checkInWarehouse"))
+            {
+                MelonLogger.Msg("Creating checkInWarehouse shortcut");
+                melonPreferencesCategory.CreateEntry("checkInWarehouse", "G");
+                melonPreferencesCategory.CreateEntry("raiseLowerLift1", "B");
+            }
+
+            base.OnApplicationStart();
+        }
+    }
+}
