d0ad077f611d50cf0413516d5f45c3dbe9d0285a
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2021-09-10 18:42:15 -0600

    Completely overhaul BW with new UI/condition count
    * remove shortcut
    * add new UI to Parts Inspector
    * add conditions and count of item in warehouse with each color

--- BetterWarehousesMod/BetterWarehouses/Loader.cs
+++ BetterWarehousesMod/BetterWarehouses/Loader.cs
@@ -1,122 +1,193 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-// using Harmony;
+using CMS.UI;
+using CMS.UI.Windows;
 using HarmonyLib;
+using Il2CppSystem.Collections.Generic;
 using MelonLoader;
-using UnhollowerRuntimeLib;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
-using Object = UnityEngine.Object;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; // using Harmony;
 
 namespace BetterWarehouses
 {
-    [HarmonyPatch(typeof(GameScript), "Update", new Type[] { })]
-    public class Patch
+    public class BaseItemWithData
+    {
+        public int Count = 0;
+        public BaseItem Item;
+        public int NumFullQuality = 0;
+        public int NumGreenQuality = 0;
+        public int NumYellowQuality = 0;
+        public int NumOrangeQuality = 0;
+        public int NumRedQuality = 0;
+
+        public BaseItemWithData(BaseItem baseItem)
+        {
+            Item = baseItem;
+        }
+    }
+
+    [HarmonyPatch]
+    public static class Patch
     {
         private static bool _warehouseInfoCreated = false;
         private static GameObject _warehouseInfo = null;
+        private static GameObject _warehouse100PercentItems = null;
+        private static GameObject _warehouseGreenItems = null;
+        private static GameObject _warehouseYellowItems = null;
+        private static GameObject _warehouseOrangeItems = null;
+        private static GameObject _warehouseRedItems = null;
         private static Text _warehouseTextComponent = null;
+        private static Text _warehouse100PercentTextComponent = null;
+        private static Text _warehouseGreenTextComponent = null;
+        private static Text _warehouseYellowTextComponent = null;
+        private static Text _warehouseOrangeTextComponent = null;
+        private static Text _warehouseRedTextComponent = null;
         private static Warehouse _warehouse = null;
-        private static KeyCode _checkInWarehouseShortcut;
-
-        private static void SetShortcut()
+        private static List<BaseItem> _warehouseItems;
+        private static System.Collections.Generic.Dictionary<string, BaseItemWithData> _warehouseIdMap;
+        private static bool isWindowVisible = false;
+        
+        private static void CreateWarehouseStockUI()
         {
-            var betterWarehousePrefs = MelonPreferences.GetCategory("BetterWarehouse");
-            var checkInWarehouseEntry = betterWarehousePrefs.GetEntry<string>("checkInWarehouse");
-            if (Enum.TryParse(checkInWarehouseEntry.Value, true, out KeyCode key))
-            {
-                _checkInWarehouseShortcut = key;
-            }
-            else
-            {
-                MelonLogger.Msg($"Unable to parse checkInWarehouse shortcut as a key. {checkInWarehouseEntry.Value}");
-                MelonLogger.Msg("Setting checkInWarehouse shortcut to default Z");
-                _checkInWarehouseShortcut = KeyCode.Z;
-            }
-                
-            // var conditionGameObject = UIManager.Get().PartInspector.condition.gameObject;
-            // var inspectorTransformParent = UIManager.Get().PartInspector.gameObject.transform.Find("Inspector");
-            // warehouseInfo = Instantiate(conditionGameObject, inspectorTransformParent);
-            // warehouseInfo.name = "warehouseInfo";
-            // MelonLogger.Msg("finished setting name");
-            // warehouseInfo.transform.position += Vector3.up * 80.0f;
-            // warehouseInfo.GetComponent<Image>().color = new Color(0.6604f, 1, 0.9812f, 1);
+            var conditionGameObject = UIManager.Get().PartInspector.condition.gameObject;
+            var inspectorTransformParent = UIManager.Get().PartInspector.gameObject.transform.Find("Inspector");
+            _warehouseInfo = GameObject.Instantiate(conditionGameObject, inspectorTransformParent);
+            _warehouse100PercentItems = GameObject.Instantiate(conditionGameObject, inspectorTransformParent);
+            _warehouseGreenItems = GameObject.Instantiate(conditionGameObject, inspectorTransformParent);
+            _warehouseYellowItems = GameObject.Instantiate(conditionGameObject, inspectorTransformParent);
+            _warehouseOrangeItems = GameObject.Instantiate(conditionGameObject, inspectorTransformParent);
+            _warehouseRedItems = GameObject.Instantiate(conditionGameObject, inspectorTransformParent);
 
-            // warehouseTextComponent = warehouseInfo.transform.Find("ConditionPercentage").GetComponent<Text>();
+            _warehouseInfo.name = "warehouseInfo";
+            _warehouseInfo.transform.position += Vector3.up * 240.0f;
+            _warehouseInfo.GetComponent<RectTransform>().sizeDelta += 3 * Vector2.up;
+            _warehouseInfo.GetComponent<Image>().color = new Color(0.6604f, 1, 0.9812f, 1);
 
-            _warehouse = GameScript.Get().gameObject.GetComponent<Warehouse>();
+            _warehouse100PercentItems.name = "warehouse100PercentInfo";
+            _warehouse100PercentItems.transform.position += Vector3.up * 200.0f;
+            _warehouse100PercentItems.GetComponent<Image>().color = new Color(0.04f, 1f, 0.09f);
+
+            _warehouseGreenItems.name = "warehouseGreenItems";
+            _warehouseGreenItems.transform.position += Vector3.up * 169.0f;
+            _warehouseGreenItems.GetComponent<Image>().color = new Color(0f, 1f, 0.51f);
+            // _warehouseGreenItems.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 1.0f);
+            // _warehouseGreenItems.GetComponent<RectTransform>().ForceUpdateRectTransforms();
+
+
+            _warehouseYellowItems.name = "warehouseYellowItems";
+            _warehouseYellowItems.transform.position += Vector3.up * 138.0f;
+            _warehouseYellowItems.GetComponent<Image>().color = new Color(1f, 0.94f, 0f);
+
+            _warehouseOrangeItems.name = "warehouseOrangeItems";
+            _warehouseOrangeItems.transform.position += Vector3.up * 107.0f;
+            _warehouseOrangeItems.GetComponent<Image>().color = new Color(1f, 0.61f, 0f);
+
+            _warehouseRedItems.name = "warehouseRedItems";
+            _warehouseRedItems.transform.position += Vector3.up * 76.0f;
+            _warehouseRedItems.GetComponent<Image>().color = new Color(1f, 0.03f, 0f);
+
+            _warehouseTextComponent = _warehouseInfo.transform.Find("ConditionPercentage").GetComponent<Text>();
+            _warehouse100PercentTextComponent =
+                _warehouse100PercentItems.transform.Find("ConditionPercentage").GetComponent<Text>();
+            _warehouseGreenTextComponent =
+                _warehouseGreenItems.transform.Find("ConditionPercentage").GetComponent<Text>();
+            _warehouseYellowTextComponent =
+                _warehouseYellowItems.transform.Find("ConditionPercentage").GetComponent<Text>();
+            _warehouseOrangeTextComponent =
+                _warehouseOrangeItems.transform.Find("ConditionPercentage").GetComponent<Text>();
+            _warehouseRedTextComponent = _warehouseRedItems.transform.Find("ConditionPercentage").GetComponent<Text>();
+
             _warehouseInfoCreated = true;
         }
 
-        private static List<IGrouping<string, BaseItem>> searchWarehouse()
+        private static BaseItemWithData SearchWarehouse()
         {
-            
             var internalPart = GameScript.Get().GetPartMouseOver();
             var bodyPart = GameScript.Get().GetIOMouseOverCarLoader();
-            
-            var warehouseFinds = new List<BaseItem>();
+
             if (internalPart != null)
             {
                 var id = internalPart.GetIDWithTuned();
-                var allWarehouseItems = _warehouse.GetAllItemsAndGroups();
-                foreach (var warehouseItem in allWarehouseItems)
-                {
-                    // MelonLogger.Msg($"warehouse item {warehouseItem.GetLocalizedName()}");
-                    if (warehouseItem.ID == id)
-                    {
-                        warehouseFinds.Add(warehouseItem);
-                    }
-                }
+                return _warehouseIdMap.ContainsKey(id) ? _warehouseIdMap[id] : new BaseItemWithData(new BaseItem());
             }
             else if (bodyPart != null)
             {
-                // var id = bodyPart.GetIDWithTuned();
-                // var allWarehouseItems = warehouse.GetAllItemsAndGroups();
-                // foreach (var warehouseItem in allWarehouseItems)
-                // {
-                //     if (warehouseItem.ID == id)
-                //     {       
-                //         warehouseFinds.Add(warehouseItem);
-                //     }
-                // }
+                // can't do anything with bodyParts right now
             }
-            
-            var warehouseFindsByCount = warehouseFinds.GroupBy(i => i.ID).ToList();
-            return warehouseFindsByCount;
+
+            return new BaseItemWithData(new BaseItem());
         }
-        
+
         [HarmonyPostfix]
-        public static void Update()
+        [HarmonyPatch(typeof(GameScript), "Update", new Type[] { })]
+        private static void Update()
         {
-            var currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+            var currentScene = SceneManager.GetActiveScene();
 
             if (currentScene.name != "garage") return; // skip if we're not in the garage
-            
-            if (_warehouseInfoCreated != true)
-            {
-                SetShortcut();
-            }
+            if (isWindowVisible) return; // skip if inventory or warehouses etc are open
 
-            if (Input.GetKeyDown(_checkInWarehouseShortcut))
+            if (_warehouseInfoCreated != true) CreateWarehouseStockUI();
+
+            var item = SearchWarehouse();
+            _warehouseTextComponent.text = $"Warehouse Total: {item.Count}";
+            _warehouse100PercentTextComponent.text = $"100%: {item.NumFullQuality}";
+            _warehouseGreenTextComponent.text = $"80%+: {item.NumGreenQuality}";
+            _warehouseYellowTextComponent.text = $"50%+: {item.NumYellowQuality}";
+            _warehouseOrangeTextComponent.text = $"15%+: {item.NumOrangeQuality}";
+            _warehouseRedTextComponent.text = $"0%+: {item.NumRedQuality}";
+        }
+
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(WarehouseWindow), "OnWindowActiveStateChange")]
+        private static void OnWindowActiveStateChange(WindowID windowID, bool stateChange)
+        {
+            MelonLogger.Msg("Hiding the warehouse window and updating warehouse contents");
+            UpdateMap();
+        }
+
+        private static void UpdateMap()
+        {
+            _warehouseItems = _warehouse.GetAllItemsAndGroups();
+            _warehouseIdMap = new System.Collections.Generic.Dictionary<string, BaseItemWithData>();
+            foreach (var item in _warehouseItems)
             {
-                var warehouseFindsByCount = searchWarehouse();
-                foreach (var grouping in warehouseFindsByCount.Where(grouping => grouping.Any()))
+                var baseItemWithData = _warehouseIdMap.ContainsKey(item.ID)
+                    ? _warehouseIdMap[item.ID]
+                    : new BaseItemWithData(item);
+                var condition = item.GetCondition();
+                switch (condition)
                 {
-                    UIManager.Get().ShowPopup("Warehouse", $"{grouping.Count()} in warehouse", PopupType.Buy);
-                    // warehouseTextComponent.text =
-                    // $"{grouping.Count()} {grouping.First().GetLocalizedName()} in Warehouse";
+                    case var _ when condition >= .99:
+                        baseItemWithData.NumFullQuality++;
+                        break;
+                    case var _ when condition >= .80:
+                        baseItemWithData.NumGreenQuality++;
+                        break;
+                    case var _ when condition >= .50:
+                        baseItemWithData.NumYellowQuality++;
+                        break;
+                    case var _ when condition > 0:
+                        baseItemWithData.NumOrangeQuality++;
+                        break;
+                    default:
+                        baseItemWithData.NumRedQuality++;
+                        break;
                 }
 
-                if (!warehouseFindsByCount.Any())
-                {
-                    UIManager.Get().ShowPopup("Warehouse", $"0 in warehouse", PopupType.Buy);
-                }
+                baseItemWithData.Count++;
+                _warehouseIdMap[item.ID] = baseItemWithData;
             }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Warehouse), "Load")]
+        private static void WarehouseLoad()
+        {
+            _warehouse = GameScript.Get().gameObject.GetComponent<Warehouse>();
+            MelonLogger.Msg("Warehouse loading");
+            UpdateMap();
         }
     }
 
@@ -124,13 +195,8 @@ namespace BetterWarehouses
     {
         public override void OnApplicationStart()
         {
-            MelonLogger.Msg("Loading preferences");
-            var melonPreferencesCategory = MelonPreferences.CreateCategory("BetterWarehouse");
-            if (!melonPreferencesCategory.HasEntry("checkInWarehouse"))
-            {
-                MelonLogger.Msg("Creating checkInWarehouse shortcut");
-                melonPreferencesCategory.CreateEntry("checkInWarehouse", "Z");
-            }
+            // MelonLogger.Msg("Loading preferences");
+            // var melonPreferencesCategory = MelonPreferences.CreateCategory("BetterWarehouse");
 
             base.OnApplicationStart();
         }
